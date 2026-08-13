d81bbceca37c92d80e600e7d1d095caa2c19fc33
divyansh jha <divyansh.j25604@nst.rishihood.edu.in>
2026-08-13 21:28:46 +0530

    algorithm

--- algorithms/config.py
+++ algorithms/config.py
@@ -1,2 +1,3 @@
 degree_of_randomness = 0.2 
+total_episodes = 1000
 
--- algorithms/main.py
+++ algorithms/main.py
@@ -1,2 +1,14 @@
-from config import allowed_actions
+import environment
+import algorithms
+import utils
+import config
+
+for episode in range(config.total_episodes):
+    env = environment.Environment(5)
+    # 
+    env.init_grid()
+
+
+
+
 
